9ebb906e7ef7455ae4b720f4802cb6ffef17fa89
Chris Barcroft <chrisabarcroft@gmail.com>
2025-02-28 23:17:02 -0800

    Add main, dockerfile, requirements, github workflow

---
+++ docker/download_model.py
@@ -0,0 +1,25 @@
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+import os
+import requests
+from huggingface_hub import configure_http_backend
+
+
+def backend_factory() -> requests.Session:
+    session = requests.Session()
+    session.verify = False
+    return session
+
+configure_http_backend(backend_factory=backend_factory)
+
+def download_model(model_path, model_name):
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+    model = DistilBertForSequenceClassification.from_pretrained(model_name)
+
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
+
+
+download_model("models/distilbert/", "distilbert-base-uncased-finetuned-sst-2-english")
---
+++ main.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+print("Initialize tokenizer...")
+tokenizer = DistilBertTokenizer.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
+)
+print("Initialize model...")
+
+model = DistilBertForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
+)
+
+print("Tokenize input text...")
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+
+print("Predict...")
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+print("Get results...")
+predicted_class_id = logits.argmax().item()
+res = model.config.id2label[predicted_class_id]
+
+print(res)
