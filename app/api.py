abc4eead864466b7243b1b7d84c5388b6a22aa50
Chris Barcroft <chrisabarcroft@gmail.com>
2025-03-01 18:27:05 -0800

    Wrap in FastAPI, split model and app requirements.txt

---
+++ app/api.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+sentiment_pipeline = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+print(sentiment_pipeline("Model initialized successfully."))
+
+app = FastAPI()
+
+class InferenceRequestModel(BaseModel):
+   input: str
+
+@app.post("/infer")
+def infer(request: InferenceRequestModel):
+	sentiment_response = sentiment_pipeline(request.input)
+
+	return sentiment_response
